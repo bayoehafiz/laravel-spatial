4d9e0bd18f74b11d8bf5c5375320d2f9a1e480c8
Bayu Hafiz <bayuhafiz@hotmail.com>
2020-12-30 11:32:48 +0700

    Remove DB injection

--- src/SpatialServiceProvider.php
+++ src/SpatialServiceProvider.php
@@ -27,20 +27,6 @@ class SpatialServiceProvider extends DatabaseServiceProvider
      */
     public function register()
     {
-        // The connection factory is used to create the actual connection instances on
-        // the database. We will inject the factory into the manager so that it may
-        // make the connections while they are actually needed and not of before.
-        $this->app->singleton('db.factory', function ($app) {
-            return new ConnectionFactory($app);
-        });
-
-        // The database manager is used to resolve various connections, since multiple
-        // connections might be managed. It also implements the connection resolver
-        // interface which may be used by other components requiring connections.
-        $this->app->singleton('db', function ($app) {
-            return new DatabaseManager($app, $app['db.factory']);
-        });
-
         if (class_exists(DoctrineType::class)) {
             // Prevent geometry type fields from throwing a 'type not found' error when changing them
             $geometries = [
