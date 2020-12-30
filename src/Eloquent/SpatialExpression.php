843e3df8cbc7a21db5b39fc8cbe1efff72b77617
Bayu Hafiz <23627311+bayoehafiz@users.noreply.github.com>
2020-12-30 15:15:31 +0700

    Remove 'axis-order=long-lat' that failed on MariaDB

--- src/Eloquent/SpatialExpression.php
+++ src/Eloquent/SpatialExpression.php
@@ -4,11 +4,14 @@ namespace Grimzy\LaravelMysqlSpatial\Eloquent;
 
 use Illuminate\Database\Query\Expression;
 
+/**
+ * Removes the ST_GeomFromText 'axis-order=long-lat' argument that fails on MariaDB.
+ */
 class SpatialExpression extends Expression
 {
     public function getValue()
     {
-        return "ST_GeomFromText(?, ?, 'axis-order=long-lat')";
+        return "ST_GeomFromText(?, ?)";
     }
 
     public function getSpatialValue()
